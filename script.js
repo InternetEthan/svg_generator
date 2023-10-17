f25647338ddf4ae3110991086c521eabf7f3b84b
InternetEthan <internetethan@gmail.com>
2023-10-17 16:53:27 -0500

    added shapes.js and moved generateSVG function to this file

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateSVG = require('./lib/shapes.js'); // Import the function
 
-function generateSVG(text, textColor, shape, shapeColor) {
-    let shapeElement;
+function createOutputFolder() {
+  const folderName = 'output';
 
-  switch (shape) {
-    case 'circle':
-      shapeElement = `
-      <circle cx="50%" cy="50%" r="35%" fill="${shapeColor}" />
-      `;
-      break;
-    case 'triangle':
-      shapeElement = `
-        <polygon points="150,0 250,150 50,150" fill="${shapeColor}" />
-      `;
-      break;
-    case 'square':
-    default:
-      shapeElement = `
-      <rect width="100%" height="100%" fill="${shapeColor}" />
-      `;
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+    console.log(`Created ${folderName} folder.`);
   }
-
-  return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-      ${shapeElement}
-      <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="24" fill="${textColor}">${text}</text>
-    </svg>
-  `;
 }
 
 inquirer.prompt([
@@ -63,12 +44,3 @@ inquirer.prompt([
     });
   })
   .catch(error => console.log(error));
-
-  function createOutputFolder() {
-    const folderName = 'output';
-  
-    if (!fs.existsSync(folderName)) {
-      fs.mkdirSync(folderName);
-      console.log(`Created ${folderName} folder.`);
-    }
-  }
