b1d352994c3246add95c9cdb2484e07af9a5467d
InternetEthan <internetethan@gmail.com>
2023-10-17 16:41:58 -0500

    added switch to change the shape based on user input

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 function generateSVG(text, textColor, shape, shapeColor) {
-  return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+    let shapeElement;
+
+  switch (shape) {
+    case 'circle':
+      shapeElement = `
+      <circle cx="50%" cy="50%" r="35%" fill="${shapeColor}" />
+      `;
+      break;
+    case 'triangle':
+      shapeElement = `
+        <polygon points="150,0 250,150 50,150" fill="${shapeColor}" />
+      `;
+      break;
+    case 'square':
+    default:
+      shapeElement = `
       <rect width="100%" height="100%" fill="${shapeColor}" />
+      `;
+  }
+
+  return `
+    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+      ${shapeElement}
       <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="24" fill="${textColor}">${text}</text>
     </svg>
   `;
@@ -22,9 +42,11 @@ inquirer.prompt([
     message: 'Enter text color (example: #FF0000):'
   },
   {
-    type: 'input',
+    type: 'list',
     name: 'shape',
-    message: 'Enter logo shape (example: circle, square):'
+    message: 'Enter logo shape (example: circle, square):',
+    choices: ['square', 'circle', 'triangle'],
+    default: 'square'
   },
   {
     type: 'input',
